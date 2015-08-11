c69d6fce079c44cdc23cf3cba38677c473b1874e
Chris Violette <cvioletterun@gmail.com>
2015-08-11 09:51:25 -0400

    Added smooth scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -29,6 +29,18 @@
     'home': {
       init: function() {
         // JavaScript to be fired on the home page
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 250);
+              return false;
+            }
+          }
+        });
       },
       finalize: function() {
         // JavaScript to be fired on the home page, after the init JS
